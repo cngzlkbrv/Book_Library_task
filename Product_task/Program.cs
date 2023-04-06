c21af036d51c703d0445e7f6901d3312cd7d5096
Cingiz <125980035+cngzlkbrv@users.noreply.github.com>
2023-04-06 15:36:55 +0400

    Update Program.cs
    run update

--- Product_task/Program.cs
+++ Product_task/Program.cs
@@ -4,11 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("Sefillerin macerasi","Filankes",100,10);
+          Book book1 = new Book("Sefillerin macerasi","Filankes",100,10);
             Book book2 = new Book("Sefillerin olumu", "Filankesli", 50, 5);
             Library library = new Library();
             library.AddBook(book1);
             library.AddBook(book2);
+            library.AllBooks();
+            library.GetBook(1);
         }
     }
 }
